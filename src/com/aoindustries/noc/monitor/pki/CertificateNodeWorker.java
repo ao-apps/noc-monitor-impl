8aac0b433b598574e2229623287a5bfb11f07439
Dan Armstrong <dan@aoindustries.com>
2019-07-29 00:13:14 +0000

    Renamed AOSH command "check_ssl_certificate" to "pki.Certificate.check", and it now accepts a boolean flag to enable/disable cached results.

--- src/com/aoindustries/noc/monitor/pki/CertificateNodeWorker.java
+++ src/com/aoindustries/noc/monitor/pki/CertificateNodeWorker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 by AO Industries, Inc.,
+ * Copyright 2018, 2019 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
@@ -122,7 +122,7 @@ class CertificateNodeWorker extends TableResultNodeWorker<List<Certificate.Check
 
 	@Override
 	protected List<Certificate.Check> getQueryResult() throws Exception {
-		return sslCertificate.check();
+		return sslCertificate.check(true);
 	}
 
 	@Override
