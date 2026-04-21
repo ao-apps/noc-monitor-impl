0b26fa184faaf74732ef7f1c6a80e3983be4769c
AO Industries, Inc <support@aoindustries.com>
2026-04-21 13:14:17 -0500

    TODO: Monitor pg_stat_checkpointer in PostgreSQL 17 and above.

--- src/main/java/com/aoindustries/noc/monitor/MonitorImpl.java
+++ src/main/java/com/aoindustries/noc/monitor/MonitorImpl.java
@@ -176,7 +176,9 @@ import java.util.Locale;
  * Monitor for certificates issued in domains that are not expected.
  *     Jonathon Moldenhaur described how there are lists of certificates issued.
  *
- * Monitor pg_stat_io in PostgreSQL 16 and above.</pre>
+ * Monitor pg_stat_io in PostgreSQL 16 and above.
+ *
+ * Monitor pg_stat_checkpointer in PostgreSQL 17 and above.</pre>
  *
  * @author  AO Industries, Inc.
  */
